be7511a5b77dcf366bdfcc4d13533efd62f60059
Nidhi Gupta <nidhigupta7877@gmail.com>
2021-07-06 22:03:07 +0530

    Initial commit

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,10 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+
+    public int getNumber(String name){
+        int num = name.length();
+        
+        return num;
+    }
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,24 @@
 package co.interleap.courses.tdd;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void isEmptyString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        assertEquals(0, vowelCounter.getNumber(""));
+    }
+
+    @Test
+    public void singleVowel(){
+        assertEquals(1,new VowelCounter().getNumber("e"));
+    }
+
+    @Test
+    public void multipleVowel(){
+        assertEquals(4,new VowelCounter().getNumber("aaii"));
+    }
+
 }
